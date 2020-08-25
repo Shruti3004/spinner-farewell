9ba3a975984647c587879dc5562629cd1d11c04d
Shruti3004 <mail2shruti.ag@gmail.com>
2020-08-25 09:16:19 +0530

    dare-spinner

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+(function () {
+  const wheel = document.querySelector(".wheel");
+  const startButton = document.querySelector(".button");
+  let deg = 0;
+
+  startButton.addEventListener("click", () => {
+    startButton.style.pointerEvents = "none";
+    deg = Math.floor(5000 + Math.random() * 5000);
+    wheel.style.transition = "all 10s ease-out";
+    wheel.style.transform = `rotate(${deg}deg)`;
+    // wheel.classList.add('blur');
+  });
+
+  wheel.addEventListener("transitionend", () => {
+    // wheel.classList.remove('blur');
+    startButton.style.pointerEvents = "auto";
+    // Need to set transition to none as we want to rotate instantly
+    wheel.style.transition = "none";
+    // Calculate degree on a 360 degree basis to get the "natural" real rotation
+    // Important because we want to start the next spin from that one
+    // Use modulus to get the rest value from 360
+    const actualDeg = deg % 360;
+    // Set the real rotation instantly without animation
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
+  });
+})();
