452aa5d08d111c0a4cc85d98764de1a2a3437a17
Shruti3004 <mail2shruti.ag@gmail.com>
2020-08-26 12:43:26 +0530

    fixed position of wheel

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@
   startButton.addEventListener("click", () => {
     startButton.style.pointerEvents = "none";
     deg = Math.floor(5000 + Math.random() * 5000);
-    wheel.style.transition = "all 10s ease-out";
+    wheel.style.transition = "all 5s ease-out";
     wheel.style.transform = `rotate(${deg}deg)`;
     // wheel.classList.add('blur');
   });
