ab2d8e08d4bd24109278fc96ca6ee728e57c4ab8
Shruti3004 <mail2shruti.ag@gmail.com>
2020-08-26 14:40:43 +0530

    updated script

--- scripts/script.js
+++ scripts/script.js
@@ -23,11 +23,7 @@ let i = 0;
     // wheel.classList.remove("blur");
     startButton.style.pointerEvents = "auto";
     wheel.style.transition = "none";
-    // Calculate degree on a 360 degree basis to get the "natural" real rotation
-    // Important because we want to start the next spin from that one
-    // Use modulus to get the rest value from 360
     const actualDeg = deg % 360;
-    // Set the real rotation instantly without animation
     wheel.style.transform = `rotate(${actualDeg}deg)`;
   });
 })();
