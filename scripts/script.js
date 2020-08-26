c7a35738974ff02720f60158d034f369fcd9b4ec
Shruti3004 <mail2shruti.ag@gmail.com>
2020-08-26 14:35:06 +0530

    fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,26 @@
+const degArray = [20, 30, 40];
+let i = 0;
 (function () {
+  i = localStorage.getItem("counter") || i;
   const wheel = document.querySelector(".wheel");
   const startButton = document.querySelector(".button");
   let deg = 0;
 
   startButton.addEventListener("click", () => {
+    localStorage.setItem("counter", ++i);
+    if (i === degArray.length) {
+      localStorage.setItem("counter", 0);
+      i = 0;
+    }
     startButton.style.pointerEvents = "none";
-    deg = Math.floor(5000 + Math.random() * 5000);
+    //deg = Math.floor(5000 + Math.random() * 5000);
     wheel.style.transition = "all 5s ease-out";
-    wheel.style.transform = `rotate(${deg}deg)`;
-    // wheel.classList.add('blur');
+    wheel.style.transform = `rotate(${degArray[i]}deg)`;
+    // wheel.classList.add("blur");
   });
 
   wheel.addEventListener("transitionend", () => {
-    // wheel.classList.remove('blur');
+    // wheel.classList.remove("blur");
     startButton.style.pointerEvents = "auto";
     wheel.style.transition = "none";
     // Calculate degree on a 360 degree basis to get the "natural" real rotation
