b9b16034f53a34bd2b4ae71eb6959842530f5239
Shruti3004 <mail2shruti.ag@gmail.com>
2020-08-28 16:29:00 +0530

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
-const degArray = [-4060, -5040, -6040, -7050, -8050, -9170];
+const degArray = [4060, 5040, 6040, 7040, 8050, 9140];
 /*
-  Rohit Jhakmola
-  Shobhit Aggarwal
-  Ayush Khare/Srivastava
-  Anant Jain
+  Rohit Jhakmola 6040 (task 3)
+  Shobhit Aggarwal  9140
+  Ayush Khare/Srivastava 4060 (task 1)
+  Anant Jain 5070
   Kunal Vishnoi
-  Rahul Singh
+  Rahul Singh 5030
+  Abhishek   9210 (task 5)
+  Ishneet Kaur 7060
+  Rohit Kumar 7040 (task 4)
+  vasu Mehra 5050
 */
 const taskArray = ["Task1", "Task2", "Task3", "Task4", "Task5", "Task6"];
 let i = 0;
@@ -38,7 +42,7 @@ let i = 0;
     // Use modulus to get the rest value from 360
     let actualDeg = degArray[i] % 360;
     // Set the real rotation instantly without animation
-    // wheel.style.transform = `rotate(${actualDeg}deg)`;
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
   });
 
   getTask.addEventListener("click", () => {
